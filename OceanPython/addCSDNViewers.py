2d047ecb7a56bf9c9abbf3790f5bd035e72332b4
fangjian <fangjian@droi.com>
2022-06-05 22:22:27 +0800

    add csdn viewer count script

---
+++ OceanPython/addCSDNViewers.py
@@ -0,0 +1,21 @@
+import random
+import time
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+BASE_URL = 'https://blog.csdn.net/weixin_44008788/article/list/'
+circle = []
+for i in range(1, 6):
+    circle.append(BASE_URL + str(i))
+
+for r in circle:
+    url = r
+    print(url)
+    browser.get(url)
+    for i in range(1, 4):
+        print(i)
+        for link in browser.find_elements_by_xpath('//h4[@class=""]/a'):
+            link.click()
+            time.sleep(random.randrange(0, 6))
+        time.sleep(random.randrange(0, 9))
